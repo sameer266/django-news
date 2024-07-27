1d1f3077e914da0c8a21ee7a4549a772ffbb480a
sameer266 <144911898+sameer266@users.noreply.github.com>
2024-07-27 16:58:52 +0545

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -10,5 +10,8 @@ echo " MAKE MIGRATIONS..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
+# List the contents of the staticfiles directory to verify
+echo "Contents of staticfiles directory:"
+ls -l staticfiles
 
 echo " BUILD END"
