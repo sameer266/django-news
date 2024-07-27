a8d8f5eb59184c256d1d21083b6c85e45fcdbd3e
sameer266 <144911898+sameer266@users.noreply.github.com>
2024-07-27 08:43:42 +0545

    Add files via upload

---
+++ build_files.sh
@@ -0,0 +1,13 @@
+echo " BUILD START"
+python3.9 -m pip install --upgrade pip
+
+python3.9 --version
+python3.9 -m ensurepip # não aparece nos tutoriais internacionais
+python3.9 -m pip install -r requirements.txt
+
+echo " MAKE MIGRATIONS..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+
+echo " BUILD END"
