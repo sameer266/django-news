05a48ca779cd7995c32ed4ddbfdd898de79fe78f
sameer266 <144911898+sameer266@users.noreply.github.com>
2024-07-27 18:02:55 +0545

    Update settings.py

--- practice/settings.py
+++ practice/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'practice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',  # Replace with your PostgreSQL database name
+        'USER': 'postgres',  # Replace with your PostgreSQL username
+        'PASSWORD': 'LXFEwbmnwRpTbsDUVKfDqBobRBRFluRb',  # Replace with your PostgreSQL password
+        'HOST': 'monorail.proxy.rlwy.net',  # Set to the hostname where your PostgreSQL server is running
+        'PORT': '32272',  # The default PostgreSQL port
     }
 }
 
