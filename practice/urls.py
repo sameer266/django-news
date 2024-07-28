297c0bf7be330c7964e661c38c87193b8e2c257f
sameer266 <144911898+sameer266@users.noreply.github.com>
2024-07-28 12:33:44 +0545

    Update urls.py

--- practice/urls.py
+++ practice/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('<news_slug>/', views.newsslugdetail, name='newshomedetail'), 
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
