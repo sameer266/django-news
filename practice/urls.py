4d3effcc911f29c90124ed972d706449cf3a98b9
sameer266 <sameerbaiju792@gmail.com>
2024-07-28 09:48:04 +0545

    changes in url slug

--- practice/urls.py
+++ practice/urls.py
@@ -7,20 +7,19 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-    path('<news_slug>/', views.newsslugdetail, name='newshomedetail'), 
+    path('register/', views.register_view, name="register"),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
+    path('login-home/', views.login_home, name="login_home"),
     path('news/', include('news.urls')),
-    
     path('newsdetail/', views.newsdetail, name="newsdetail"),
     path('newsdetail/<news_slug>/', views.newsslugdetail, name="newsslugdetail"),
     
-    # ----register----login
-    path('register/', views.register_view, name="register"),
-    path('login/', views.login_view, name="login"),
-    
-    # logout
-    path('logout/', views.logout_view, name="logout"),
-    
-    path('login-home/', views.login_home, name="login_home")
+    #  i put this url at last beacause
+    # Example Conflict:
+# If a user navigates to /register/, Django will match it with <news_slug>/ before it gets a chance to match it with path('register/', views.register_view, name="register").
+# This results in the newsslugdetail view being called with news_slug='register'.
+    path('<news_slug>/', views.newsslugdetail, name='newshomedetail'), 
 ]
 
 if settings.DEBUG:
